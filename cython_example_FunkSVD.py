f0935d7f42833446bd4403b2e8db08a5354834e5
MaurizioFD <maurizio.ferrari@polimi.it>
2021-11-10 17:48:33 +0100

    Update A.Y. 2021/22

--- cython_example_FunkSVD.py
+++ cython_example_FunkSVD.py
@@ -4,7 +4,7 @@ Created on 09/11/2020
 @author: Maurizio Ferrari Dacrema
 """
 
-from Notebooks_utils.data_splitter import train_test_holdout
+from Data_manager.split_functions.split_train_validation_random_holdout import split_train_in_two_percentage_global_sample
 from Data_manager.Movielens.Movielens10MReader import Movielens10MReader
 
 data_reader = Movielens10MReader()
@@ -12,7 +12,7 @@ data_loaded = data_reader.load_data()
 
 URM_all = data_loaded.get_URM_all()
 
-URM_train, URM_test = train_test_holdout(URM_all, train_perc = 0.8)
+URM_train, URM_test = split_train_in_two_percentage_global_sample(URM_all, train_percentage = 0.8)
 
 
 from Cython_examples.FunkSVD_fastest import train_multiple_epochs
